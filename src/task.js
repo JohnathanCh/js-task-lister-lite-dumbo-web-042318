a0f3e87d4cdbbca368fdee2d575ab3b4a1200051
github username <github email address>
2018-06-11 18:18:07 -0400

    done

--- src/task.js
+++ src/task.js
@@ -1,2 +1,57 @@
-class Task {
-}
+// taskForm.onsubmit = function(e) {alert(e.value)};
+// taskForm.onsubmit = fun()
+
+// taskForm.addEventListener('submit', submitForm)
+
+// function fun(e) {
+//   e.preventDefault();
+//   const getTask = document.getElementById("new-task-description")
+//   let value =  getTask.value
+//   console.log(e);
+//   console.log(value);
+//   createTask(value)
+//   getTask.placeholder = 'description'
+// }
+
+
+const taskForm = document.getElementById("create-task-form")
+taskForm.addEventListener('submit', submitForm)
+// taskForm.addEventListener('submit', submitForm)
+// function newTask(e) {
+//   console.log("yo")
+//   e.preventDefault();
+//   let idk = new Task;
+//   idk.submitForm(e)
+//
+// }
+
+  function submitForm(e) {
+    e.preventDefault();
+    const getTask = document.getElementById("new-task-description")
+    let value =  getTask.value
+    console.log(e);
+    console.log(value);
+    createTask(value)
+    getTask.value =  ''
+  }
+
+
+// class Task {
+//    // eventListener () {
+//    //   taskForm.addEventListener('submit', submitForm)
+//    // }
+//
+//    submitForm(e) {
+//      e.preventDefault();
+//      const getTask = document.getElementById("new-task-description")
+//      let value =  getTask.value
+//      console.log(e);
+//      console.log(value);
+//      createTask(value)
+//      getTask.placeholder = 'description'
+//    }
+//
+//
+//
+//
+// }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,24 @@
+//Lists created tasks
+const list = document.getElementById("tasks")
+console.log(list)
+
+function createTask(value) {
+  const li = document.createElement('li')
+  li.innerText =  value
+  li.className = 'list'
+  list.appendChild(li)
+
+}
+
 class TaskList {
+
+
+  addTask() {
+    //will add task based on user input after listening for a click of the submit button
+
+
+
+  }
+
+
 }
